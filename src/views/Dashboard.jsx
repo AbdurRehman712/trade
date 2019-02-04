6586f3d8becb8198b9db72317d2a47ef9063fae2
armwurfel <arm.wurfel@gmail.com>
2019-02-04 23:07:36 +0500

    currency-dashboard-layout

--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -49,6 +49,90 @@ class Dashboard extends React.Component {
     return (
       <>
         <div className="content">
+        <Row>
+            <Col lg="2">
+              <Card className="card-chart">
+                <CardHeader>
+                  <h5 className="card-category">Total Shipments</h5>
+                  <CardTitle tag="h3">
+                    <i className="tim-icons icon-bell-55 text-info" />{" "}
+                    763,215
+                  </CardTitle>
+                </CardHeader>
+                <CardBody>
+                  
+                </CardBody>
+              </Card>
+            </Col>
+            <Col lg="2">
+              <Card className="card-chart">
+                <CardHeader>
+                  <h5 className="card-category">Daily Sales</h5>
+                  <CardTitle tag="h3">
+                    <i className="tim-icons icon-delivery-fast text-primary" />{" "}
+                    3,500€
+                  </CardTitle>
+                </CardHeader>
+                <CardBody>
+
+                </CardBody>
+              </Card>
+            </Col>
+            <Col lg="2">
+              <Card className="card-chart">
+                <CardHeader>
+                  <h5 className="card-category">Completed Tasks</h5>
+                  <CardTitle tag="h3">
+                    <i className="tim-icons icon-send text-success" /> 12,100K
+                  </CardTitle>
+                </CardHeader>
+                <CardBody>
+                  
+                </CardBody>
+              </Card>
+            </Col>
+            <Col lg="2">
+              <Card className="card-chart">
+                <CardHeader>
+                  <h5 className="card-category">Total Shipments</h5>
+                  <CardTitle tag="h3">
+                    <i className="tim-icons icon-bell-55 text-info" />{" "}
+                    763,215
+                  </CardTitle>
+                </CardHeader>
+                <CardBody>
+                  
+                </CardBody>
+              </Card>
+            </Col>
+            <Col lg="2">
+              <Card className="card-chart">
+                <CardHeader>
+                  <h5 className="card-category">Daily Sales</h5>
+                  <CardTitle tag="h3">
+                    <i className="tim-icons icon-delivery-fast text-primary" />{" "}
+                    3,500€
+                  </CardTitle>
+                </CardHeader>
+                <CardBody>
+                  
+                </CardBody>
+              </Card>
+            </Col>
+            <Col lg="2">
+              <Card className="card-chart">
+                <CardHeader>
+                  <h5 className="card-category">Completed Tasks</h5>
+                  <CardTitle tag="h3">
+                    <i className="tim-icons icon-send text-success" /> 12,100K
+                  </CardTitle>
+                </CardHeader>
+                <CardBody>
+                 
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
           <Row>
             <Col xs="12">
               <Card className="card-chart">
